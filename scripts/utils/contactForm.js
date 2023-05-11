9796d26e0c67082cc36cd9fbae2bcc15c037b366
LAPTOP-LCRCBR4G\glall <g.lallemand@gmail.com>
2023-05-11 16:09:59 +0200

    modifying open and close modal functions, and add form inputs

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -2,10 +2,20 @@
 
 function displayModal() {
     const modal = document.getElementById("contact_modal");
+    const background = document.querySelector(".media-section");
+    const filterDiv=document.querySelector(".filter-div");
+
 	modal.style.display = "block";
+    background.style.filter="blur(5px)";
+    filterDiv.style.filter="blur(5px)";
 }
 
 function closeModal() {
     const modal = document.getElementById("contact_modal");
+    const background = document.querySelector(".media-section");
+    const filterDiv=document.querySelector(".filter-div");
+
     modal.style.display = "none";
+    background.style.filter="blur(0)";
+    filterDiv.style.filter="blur(0)";
 }
